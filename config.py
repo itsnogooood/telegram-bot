60fbd284db72ad13e8d267e50eb65cd1c6db0fd3
itsnogooood <56965962+itsnogooood@users.noreply.github.com>
2020-07-22 21:49:41 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from telebot import apihelper
-access_token = '1185226481:AAGuU1OiF9BcFbu6O0YxXv4nyeDTgZRV6pw'
-chat_channel = 'https://t.me/joinchat/AAAAAFSPK7ZzQ09Mru5qnA'
-apihelper.proxy = {'https': 'socks5h://user:password-eat-the-doh@proxy.pavkazzz.ru:1080'}
-global_rules = 'asd'
-rules = '''
-ПРАВИЛА ОФОРМЛЕНИЯ ПОКУПОК!
-'''
-test = 'msg'
